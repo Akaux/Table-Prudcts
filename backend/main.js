03fee86a4a24853e5de98382d67e8c5c89405524
Kauã Franklin <kauafkrocha@gmail.com>
2026-07-03 21:53:45 -0300

    1.0

--- backend/main.js
+++ backend/main.js
@@ -3,15 +3,20 @@ const valor = document.getElementById('npt-2')
 const quantidade = document.getElementById('npt-3')
 const btn1 = document.getElementById('btn-1')
 
-btn1.addEventListener('click',envia)
+class table  {
+    constructor(nome,preco,quantidade){
+        this.nome = nome,
+        this.preco = preco,
+        this.quantidade = quantidade
+    }
+}
 
+const list = [];
+
+btn1.addEventListener('click',envia)
 function envia (){
     console.log(`Nome do Produto:${nome.value}`)
-    if(valor.value >0){
-        console.log(`seu produto custa ${valor.value}`)
-    } else {
-        console.log('Seu valor é invalido !')
-    }
-
-    if(quantidade.value >0) console.log(`Sua quantidade é valida`)
+    const vA = new table(nome.value,valor.value,quantidade.value)
+    list.push(vA)
+    console.log(list)
 }
